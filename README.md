f658563dc454a30a192121dc11f8a40512250086
Diana Tuz <dianatuz12@gmail.com>
2023-09-04 19:03:39 +0300

    d

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run 🚀
+
+1. Clone repository to your computer.
+2. Turn in terminal `npm run dev`.
+3. Click in the link in the terminal.
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CustomMap from "./CustomMap";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <CustomMap />
+    </>
+  );
+}
+
+export default App;
